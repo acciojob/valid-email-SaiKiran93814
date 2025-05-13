ea18408732e9f11c181d250e61aa9c5de1370811
SaiKiran93814 <60767499+SaiKiran93814@users.noreply.github.com>
2025-05-13 11:54:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function validEmail(str) {
   //your JS code here.
+	 const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}(\.[a-zA-Z]{2})?$/;
+  
+  // Test the string against the regex and return true if valid, otherwise false
+  return regex.test(str);
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
+//const str = prompt("Enter an email address.");
 alert(validEmail(str));
